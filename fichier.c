e04245d6919c0acb114c6033d31426c579f1eb99
Pomme2T3rr3 <damian.deneuville@gmail.com>
2025-04-11 08:39:07 +0200

    fixes

--- fichier.c
+++ fichier.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int compte_ligne(FILE *f){
     int res; 
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "genetic.h"
+
+
+
+int main(void) {
+    //test de la fonction liste_visites_aleatoire
+    Visites V;
+    
+}
