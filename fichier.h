ee4ef27c584ad7c58002faa89acaf7fccd432d06
Pomme2T3rr3 <damian.deneuville@gmail.com>
2025-04-11 09:10:50 +0200

    fixes

--- fichier.h
+++ fichier.h
@@ -1,9 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "moteur.h"
 
 #ifndef FICHIER_H
 #define FICHIER_H
+
+
+Visites *text_to_struct(FILE *f);
 
 int compte_ligne(void);
 
