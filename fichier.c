45766c6393d59eb8d10e51766fa36140207a51fa
Pomme2T3rr3 <damian.deneuville@gmail.com>
2025-04-11 09:10:04 +0200

    fixe

--- fichier.c
+++ fichier.c
@@ -1,5 +1,27 @@
 #include"fichier.h"
 
+
+Visites *text_to_struct(FILE *f){
+    int i = 0;
+    char c;
+    Ville *villes = malloc(sizeof(Ville) * TAILLE_LST_VILLES);
+    Visites *V = malloc(sizeof(Visites));
+    V->lst_villes = villes;
+    V->longueur = TAILLE_LST_VILLES;
+
+    while((c=fgetc(f))!=EOF){
+        if(c =='\n'){
+            i++;
+        }else{
+            villes[i].nom = c;
+            villes[i].x = fgetc(f);
+            villes[i].y = fgetc(f);
+        }
+    }
+    return V;
+} 
+
+
 int compte_ligne(){
     int res; 
     char c;
--- moteur.c
+++ moteur.c
@@ -2,26 +2,6 @@
 #include"moteur.h"
 
 
-Visites *text_to_struct(FILE *f){
-    int i = 0;
-    char c;
-    Ville *villes = malloc(sizeof(Ville) * TAILLE_LST_VILLES);
-    Visites *V = malloc(sizeof(Visites));
-    V->lst_villes = villes;
-    V->longueur = TAILLE_LST_VILLES;
-
-    while((c=fgetc(f))!=EOF){
-        if(c =='\n'){
-            i++;
-        }else{
-            villes[i].nom = c;
-            villes[i].x = fgetc(f);
-            villes[i].y = fgetc(f);
-        }
-    }
-    return V;
-} 
-
 double longueur(Ville a , Ville b){
     return sqrt(((b.x - a.x) * (b.x - a.x)) + ((b.y - a.y) * (b.y - a.y)));
 }
